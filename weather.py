8a45908886c39386aee3661088f8ef1344e34dc5
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-10 12:35:53 +0500

    Add files via upload

--- weather.py
+++ weather.py
@@ -54,7 +54,7 @@ def processRequest(req):
         "source": "dialogflow-weather-by-satheshrgs"
         }
     
-    if __name__ == '__main__':
+if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
