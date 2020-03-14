0720955710947e1fe8fe899186efe2ffe546e425
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-14 21:37:50 +0500

    Add files via upload

--- weather.py
+++ weather.py
@@ -54,17 +54,17 @@ def processRequest(req):
             
     elif  phoneNumber == 'make' or phoneNumber == 'Make':
         if(temperature == 'hot'):
-            temperature1 = 60.0
+            temperature = 60.0
         else:
             temperature = 50.0
         if(dayCount == 'today'):
-            dayCount1 == 0
+            dayCount == 0
         else:
             dayCount == 0
         dateToday= datetime.date(datetime.now())
         url = 'http://20.46.150.26/hypernet/entity/V2/add_activity_scehdule_appliance/'
         parameterToPass = {"end_date":"2020-03-14","end_times":["11:38"],"start_times":["11:30"],
-                           "action_items":"70","primary_entity":127,"activity_route":"Dishes","activity_type":2017,"t2":60.0,"start_date":"2020-03-14","day_count":1}
+                           "action_items":"70","primary_entity":127,"activity_route":"Dishes","activity_type":2010,"t2":75.0,"start_date":"2020-03-14","day_count":1}
         request1 = requests.post(url, json = parameterToPass, headers={'Authorization': 'Token e89f01f5d23dd9c2172e788ade9f0e363190b843'})
         print(type(request1))
         requestStatus = request1.json()
