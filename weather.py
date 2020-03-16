e9b2a8be50f1e49fb01138f1c7d7fff53c4fee5b
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-16 11:02:52 +0500

    Add files via upload

--- weather.py
+++ weather.py
@@ -53,14 +53,14 @@ def processRequest(req):
             speech = "Login Failed"
             
     elif  phoneNumber == 'make' or phoneNumber == 'Make':
-##        if(temperature1 == 'hot'):
-##            temperature2 = 75.0
-##        else:
-##            temperature2 = 50.0
-##        if(dayCount1 == 'today'):
-##            dayCount2 == 1
-##        else:
-##            dayCount2 == 0
+        if(temperature1 == 'hot'):
+            temperature2 = 75.0
+        else:
+            temperature2 = 50.0
+        if(dayCount1 == 'today'):
+            dayCount2 == 1
+        else:
+            dayCount2 == 0
 ##        dateToday= datetime.date(datetime.now())
         url = 'http://20.46.150.26/hypernet/entity/V2/add_activity_scehdule_appliance/'
         parameterToPass = {"end_date":"2020-03-16","end_times":["11:38"],"start_times":["11:30"],
