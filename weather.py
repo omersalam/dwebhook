ecf8560a32080f383545884d0767d70efee456f1
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-17 15:46:51 +0500

    Add files via upload

--- weather.py
+++ weather.py
@@ -107,13 +107,14 @@ def processRequest(req):
 ##        print (datetime.now() - timedelta(hours=5))
 ##        updatedTime = (datetime.now() + timedelta(hours=5)) 
         if requestStatus['status'] == 200:
-            speech = "Total Scedule List"
+            speech = "Scedule List"
             for res in requestStatus['response']:
                 print(speech)
                 global count
                 if res['start_date'] == str(dateToday):
                     count += 1
-                    speech = speech + str(count) +  str("\n Date: "+res['start_date'] + "\n Temperature:" + res['temperature'] + "\n")  
+                    speech = speech  +  str("\n Date: "+res['start_date'] + "\n Temperature:" + res['temperature'] + "\n")
+            speech = speech + str(count)            
         else:
             speech = "Failed to fetech"
 
